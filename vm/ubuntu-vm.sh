3dc4c10123e121641b9a31696a54037c5b6e814e
rafpigna <85379052+rafpigna@users.noreply.github.com>
2023-12-05 14:53:35 +0100

    Update ubuntu-vm.sh
    fix again

--- vm/ubuntu-vm.sh
+++ vm/ubuntu-vm.sh
@@ -407,7 +407,7 @@ pvesm alloc $STORAGE $VMID $DISK0 4M 1>&/dev/null
 qm importdisk $VMID ${FILE} $STORAGE ${DISK_IMPORT:-} 1>&/dev/null
 qm set $VMID \
   -efidisk0 ${DISK0_REF}${FORMAT} \
-  -scsi0 ${DISK1_REF},${DISK_CACHE}${THIN}size=32G \
+  -scsi0 ${DISK1_REF},${DISK_CACHE}${THIN}size=2G \
   -ide2 ${STORAGE}:cloudinit \
   -boot order=scsi0 \
   -serial0 socket \
